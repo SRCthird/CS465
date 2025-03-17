5a59681b708226088760502c7e85f2edbaa10369
Stephen Chryn <SRCthird@gmail.com>
2025-03-16 21:22:56 -0400

    Move views and routes to src dir

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 
-import indexRouter from './routes/index.js';
-import usersRouter from './routes/users.js';
+import indexRouter from './src/routes/index.js';
+import usersRouter from './src/routes/users.js';
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join('views'));
+app.set('views', 'src/views');
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join('public')));
+app.use(express.static('public'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
