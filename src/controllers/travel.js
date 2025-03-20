cbfc5393a4f3c67962ca914511597fde734ef52e
Stephen Chryn <SRCthird@gmail.com>
2025-03-20 19:56:41 -0400

    Create travel hbs view

--- src/controllers/travel.js
+++ src/controllers/travel.js
@@ -1,5 +1,7 @@
+import trips from '../data/trip.json' with { type: 'json' };
+
 const travel = (req, res, next) => {
-  res.render('travel', { title: 'Travel Gateways' });
+  res.render('travel', { title: 'Travel Gateways', trips});
 };
 
 export default travel;
