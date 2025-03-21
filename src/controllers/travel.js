8d76b5d10c12357928b8f121ab5e1aea06a8e612
Stephen Chryn <SRCthird@gmail.com>
2025-03-20 20:20:49 -0400

    Correct travel title

--- src/controllers/travel.js
+++ src/controllers/travel.js
@@ -1,7 +1,7 @@
 import trips from '../data/trip.json' with { type: 'json' };
 
 const travel = (req, res, next) => {
-  res.render('travel', { title: 'Travel Gateways', trips});
+  res.render('travel', { title: 'Dive Sites - Bhaccasyoniztas Beach Resort', trips});
 };
 
 export default travel;
