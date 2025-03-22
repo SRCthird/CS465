add93b3082a1238cf88a69afa9ebe33c9fa974db
Stephen Chryn <SRCthird@gmail.com>
2025-03-22 16:47:52 -0400

    Fix the given index template

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,11 @@
+import news from '../data/news.json' with { type: 'json' };
+
 const index = (req, res, next) => {
   res.render('index', { 
-    title: 'Express',
-    active: 'index'
+    title: 'Travlr Getaways Website',
+    active: 'index',
+    // only show the second 2 stories
+    news: news.slice(1, 3)
   });
 };
 
