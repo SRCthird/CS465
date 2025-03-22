9c0121b9059190b6ff6ffba52bfd59e2b7653165
Stephen Chryn <SRCthird@gmail.com>
2025-03-22 18:57:38 -0400

    Update the travel controller to fetch from api

--- src/controllers/travel.js
+++ src/controllers/travel.js
@@ -1,11 +1,24 @@
-import trips from '../data/trip.json' with { type: 'json' };
+const travel = async (req, res, next) => {
 
-const travel = (req, res, next) => {
-  res.render('travel', {
-    title: 'Dive Sites - Bhaccasyoniztas Beach Resort',
-    active: 'travel',
-    trips
-  });
+  await fetch(
+    `${req.protocol}://${req.get('host')}/api/travel`, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json'
+      }
+    }
+  )
+    .then(response => response.json())
+    .then(json => {
+      res.render('travel', {
+        title: 'Dive Sites - Bhaccasyoniztas Beach Resort',
+        active: 'travel',
+        trips: json
+      });
+    })
+    .catch(err => {
+      res.status(500).send(err.message);
+    });
 };
 
 export default travel;
