1b2b5bc057efc7f97c8c263688a1620f389a7e4f
Stephen Chryn <SRCthird@gmail.com>
2025-03-22 18:23:44 -0400

    Define the getOne endpoint

--- api/routes/travel.js
+++ api/routes/travel.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 
-import controller from '../controllers/travel.js';
+import { getAll, getOne } from '../controllers/travel.js';
 
 var router = Router();
 
-/* GET home page. */
-router.get('/', controller);
+// GET Method for all trips
+router.get('/', getAll);
+// GET Method for one trip based off code
+router.get('/:tripCode', getOne);
 
 export default router;
