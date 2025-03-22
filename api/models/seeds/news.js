bcc29c76e4e3577c5a607e21fd018061c734b1c9
Stephen Chryn <SRCthird@gmail.com>
2025-03-22 17:55:09 -0400

    Move models and seeds to api dir

---
+++ api/models/seeds/news.js
@@ -0,0 +1,13 @@
+import Mongoose from '../../controllers/db.js';
+import News from '../news.js';
+import news from '../../data/news.json' with {type: 'json'};
+
+const seedDB = async () => {
+  await News.deleteMany({});
+  await News.insertMany(news);
+}
+
+seedDB().then(async () => {
+  await Mongoose.connection.close();
+  process.exit(0)
+})
---
+++ api/models/seeds/trip.js
@@ -0,0 +1,13 @@
+import Mongoose from '../../controllers/db.js';
+import Trip from '../trip.js';
+import trips from '../../data/trip.json' with {type: 'json'};
+
+const seedDB = async () => {
+  await Trip.deleteMany({});
+  await Trip.insertMany(trips);
+}
+
+seedDB().then(async () => {
+  await Mongoose.connection.close();
+  process.exit(0)
+})
