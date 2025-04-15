58bad18256ba4e2cc52398549d6649253258e04e
Stephen Chryn <SRCthird@gmail.com>
2025-04-15 18:17:53 -0400

    Separate trip-card into separate component

---
+++ admin/src/app/trip-card/trip-card.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-trip-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './trip-card.component.html',
+  styleUrl: './trip-card.component.css'
+})
+export class TripCardComponent {
+  @Input('trip') trip: any = {};
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
+
+}
