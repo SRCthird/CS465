35b5ed402c3b1f3b924e413ce5cfb90aec011f1f
Stephen Chryn <SRCthird@gmail.com>
2025-04-15 17:29:59 -0400

    update component import list and display data

--- admin/src/app/app.component.ts
+++ admin/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TripListingComponent } from './trip-listing/trip-listing.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, CommonModule, TripListingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
