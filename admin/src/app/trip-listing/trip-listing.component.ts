04841df35cd31255cf1ea9a9ae0613f5d74e650c
Stephen Chryn <SRCthird@gmail.com>
2025-04-15 17:29:37 -0400

    Add trip-listing data

--- admin/src/app/trip-listing/trip-listing.component.ts
+++ admin/src/app/trip-listing/trip-listing.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import trips from '../data/trip.json';
 
 @Component({
   selector: 'app-trip-listing',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './trip-listing.component.html',
   styleUrl: './trip-listing.component.css'
 })
 export class TripListingComponent {
+  trips: Array<any> = trips;
 
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
 }
