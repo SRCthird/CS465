8fedb8d765633d53c980ec4c67802ca00eb6b6e9
Stephen Chryn <SRCthird@gmail.com>
2025-04-15 18:56:01 -0400

    modify trip-list to use service

--- admin/src/app/trip-listing/trip-listing.component.ts
+++ admin/src/app/trip-listing/trip-listing.component.ts
@@ -1,21 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import trips from '../data/trip.json';
 import { TripCardComponent } from '../trip-card/trip-card.component';
+import { TripDataService } from '../services/trip-data.service';
+import Trip from '../models/trips';
 
 @Component({
   selector: 'app-trip-listing',
   standalone: true,
   imports: [CommonModule, TripCardComponent],
+  providers: [TripDataService],
   templateUrl: './trip-listing.component.html',
   styleUrl: './trip-listing.component.css'
 })
 export class TripListingComponent {
-  trips: Array<any> = trips;
+  trips: Array<Trip> = [];
+  message: string = "";
 
-  constructor() {}
+  constructor(private tripDataService: TripDataService) {
+    console.log('trip-listing constructor');
+  }
+
+  private getStuff(): void {
+    this.tripDataService.getTrips()
+      .subscribe({
+        next: (value: any) => {
+          this.trips = value;
+          if (value.length > 0) {
+            this.message = 'There are ' + value.length + ' trips available.';
+          }
+          else {
+            this.message = 'There were no trips retrieved from the database';
+          }
+          console.log(this.message);
+        },
+        error: (error: any) => {
+          console.log('Error: ' + error);
+        }
+      })
+  }
 
   ngOnInit(): void {
-
+    this.getStuff();
   }
 }
