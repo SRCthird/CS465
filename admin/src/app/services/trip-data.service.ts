824ce3d9c804b6cac2a28f5a7257e8f22638b3f2
Stephen Chryn <SRCthird@gmail.com>
2025-04-15 20:03:55 -0400

    Add trip routes

--- admin/src/app/services/trip-data.service.ts
+++ admin/src/app/services/trip-data.service.ts
@@ -12,11 +12,18 @@ export class TripDataService {
   private url = 'https://localhost:8080/api/travel';
 
   getTrips(): Observable<Trip[]> {
-
     return this.http.get<Trip[]>(this.url);
+  }
+
+  getTrip(formData: Trip): Observable<Trip[]> {
+    return this.http.get<Trip[]>(this.url + '/' + formData.code);
   }
 
   addTrip(formData: Trip): Observable<Trip[]> {
     return this.http.post<Trip[]>(this.url, formData);
+  }
+
+  updateTrip(formData: Trip): Observable<Trip[]> {
+    return this.http.put<Trip[]>(this.url + '/' + formData.code, formData)
   }
 }
